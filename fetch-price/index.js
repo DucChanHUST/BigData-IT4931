9714f5f18959d3edf15de2f29eb314fe811251cb
DucChanHUST <kuntakufcvn@gmail.com>
2024-11-28 01:03:21 +0700

    fetch price

--- fetch-price/index.js
+++ fetch-price/index.js
@@ -1,63 +1,13 @@
-import axios from "axios";
-import { ethers } from "ethers";
+import { MongoService } from "./service/mongo.js";
+import { TokenPriceService } from "./service/tokenPriceService.js";
 
-const provider = new ethers.providers.JsonRpcProvider(
-  "https://eth.llamarpc.com"
-);
+const start = async () => {
+  const mongoService = new MongoService();
+  const tokenPriceService = new TokenPriceService();
 
-const getBlockTimestamp = async (blockNumber) => {
-  try {
-    const block = await provider.getBlock(blockNumber);
-    return block.timestamp;
-  } catch (error) {
-    console.error("Error in getBlockTimestamp", error);
-  }
+  await mongoService.connectMongo();
+  
+  await tokenPriceService.fetchPrice();
 };
 
-const getTokenPriceAtTimeStamp = async (
-  tokenId,
-  timestampFrom,
-  timestampTo
-) => {
-  try {
-    const url = `https://api.coingecko.com/api/v3/coins/${tokenId}/market_chart/range?vs_currency=usd&from=${timestampFrom}&to=${timestampTo}`;
-
-    const response = await axios.get(url);
-    if (!response.data || !response.data.prices) {
-      throw new Error("No data found");
-    }
-
-    return response.data.prices;
-  } catch (error) {
-    console.error("Error in getTokenPriceAtTimeStamp", error);
-  }
-};
-
-const fetchPrice = async () => {
-  try {
-    const tokenId = "ethereum";
-    const blockNumberTo = await provider.getBlockNumber();
-    console.log("blockNumberTo", blockNumberTo);
-
-    const blockNumberFrom = blockNumberTo - 50;
-    console.log("blockNumberFrom", blockNumberFrom);
-
-    const timestampTo = await getBlockTimestamp(blockNumberTo);
-    console.log("timestampTo", timestampTo);
-
-    const timestampFrom = await getBlockTimestamp(blockNumberFrom);
-    console.log("timestampFrom", timestampFrom);
-
-    const prices = await getTokenPriceAtTimeStamp(
-      tokenId,
-      // timestampFrom,
-      timestampTo,
-      timestampTo
-    );
-    console.log(prices);
-  } catch (error) {
-    console.error("Error in fetchPrice", error);
-  }
-};
-
-fetchPrice();
+await start();
